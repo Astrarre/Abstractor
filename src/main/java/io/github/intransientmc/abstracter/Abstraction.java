2bafd0957fd35b2788136279e33ecd68dd483199
Devan-Kerman <devan@cleverpath.com>
2020-05-01 09:56:06 -0500

    wip

---
+++ src/main/java/io/github/intransientmc/abstracter/Abstraction.java
@@ -0,0 +1,7 @@
+package io.github.intransientmc.abstracter;
+
+public class Abstraction {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/main/java/io/github/intransientmc/abstracter/emitter/InterfaceEmitter.java
@@ -0,0 +1,7 @@
+package io.github.intransientmc.abstracter.emitter;
+
+import org.objectweb.asm.tree.ClassNode;
+import java.util.function.Function;
+
+public class InterfaceEmitter {
+}
